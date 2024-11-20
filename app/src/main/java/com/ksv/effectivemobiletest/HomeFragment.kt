a557c50385436f634c68e251b45a73a6634247c0
KotlyarovSergey <zews-sv@rambler.ru>
2024-11-20 16:23:35 +0300

    intermediate commit

--- app/src/main/java/com/ksv/effectivemobiletest/HomeFragment.kt
+++ app/src/main/java/com/ksv/effectivemobiletest/HomeFragment.kt
@@ -21,9 +21,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.title.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_courseFragment)
-        }
+
     }
 
     override fun onDestroyView() {
